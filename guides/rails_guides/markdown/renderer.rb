a58e3f4ae8913fff6a39ff3704df3cd5e2b8bcc1
Hyo Seong Choi <rorlab@gmail.com>
2013-05-13 17:00:07 +0900

    updated original link tag (one·all)

--- guides/rails_guides/markdown/renderer.rb
+++ guides/rails_guides/markdown/renderer.rb
@@ -42,7 +42,7 @@ HTML
 
         def convert_original(text)
           text.gsub(/^(.+)\[\[\[(.+)\]\]\]$/) do
-            linkback = %(<a href="#" class="original-link" onclick="$(this).parent().parent().next().toggle();return false;">[원문보기]</a>)
+            linkback = %(<a href="#" class="original-link" onclick="$(this).parent().parent().next().toggle();return false;">[원문</a>·<a href='#' class="original-link" onclick="$('.original-text').toggle();return false;">전체]</a>)
             %(<p>#{$1} <sup>#{linkback}</sup></p><p class="original-text">#{$2}</p>)
           end
         end
